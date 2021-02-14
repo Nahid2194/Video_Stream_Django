eb5210ebbf1aca2ee39e7f2965e069ab0b1f381d
Nahid2194 <nahidjc80@gmail.com>
2021-02-14 20:59:18 +0600

    Models adding Login App

--- Login_App/models.py
+++ Login_App/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = models.ImageField(
+        upload_to='profile_picture', blank=True)
+    about = models.CharField(blank=True, max_length=250)
+    full_name = models.CharField(max_length=255, blank=True)
+    website = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.user.username
