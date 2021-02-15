24e9dbea9d06d90913b75f6e68f72f488ed3b7cf
Nahid2194 <nahidjc80@gmail.com>
2021-02-15 10:29:47 +0600

    signup user save database

--- Login_App/views.py
+++ Login_App/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from Login_App.forms import LoginForm, SignupForm, EditProfileForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -10,8 +11,9 @@ def signup_user(request):
         form = SignupForm(data=request.POST)
         if form.is_valid():
             user = form.save()
-            user_profile = Profile(user)
+            user_profile = Profile(user=user)
             user_profile.save()
+            return HttpResponseRedirect(reverse('Login_App:login'))
     return render(request, 'Login_App/signup.html', context={'form': form})
 
 
@@ -19,9 +21,11 @@ def login_user(request):
     return render(request, 'Login_App/login.html')
 
 
+@login_required
 def logout_user(request):
     return render(request, 'Login_App/signup.html')
 
 
+@login_required
 def profile(request):
     return render(request, 'Login_App/profile.html')
