89c6252c16d3cff4c34edfeb28762c6e9b57d654
Nahid2194 <nahidjc80@gmail.com>
2021-02-15 23:05:26 +0600

    video uploading model creating

--- Stream_App/models.py
+++ Stream_App/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.utils.text import slugify
+from django.contrib.auth.models import User
+import uuid
 # Create your models here.
+
+
+class Video(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='post_user')
+    video_title = models.CharField(max_length=250, verbose_name='Put a Title')
+    slug = models.SlugField(max_length=5, unique=True)
+    video_content = models.FileField(
+        verbose_name='Upload Video', upload_to='videos')
+    thumbnail = models.ImageField(
+        verbose_name='Add to Thumbnail', upload_to='thumbnails', blank=True)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-publish_date']
+
+    def __str__(self):
+        return self.video_title
+
+    def save(self):
+        self.slug = slugify(self.video_title + '-' + str(uuid.uuid4()))
+        super(Video, self).save()
