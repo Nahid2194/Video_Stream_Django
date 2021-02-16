340a2e8eed188c628d8246c4738753bac7d3406c
Nahid2194 <nahidjc80@gmail.com>
2021-02-16 12:21:28 +0600

    upload videos view adding

--- Stream_App/views.py
+++ Stream_App/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 from .forms import CommentForm, VideoForm
 import uuid
+import uuid4
 from .models import Comment, Video
 # Create your views here.
 
@@ -12,8 +13,19 @@ def home(request):
     return render(request, 'Stream_App/home.html', context={})
 
 
+@login_required
 def upload_videos(request):
-    return render(request, 'Stream_App/home.html')
+    form = VideoForm()
+    if request.method == 'POST':
+        form = VideoForm(request.POST, request.FILES)
+        if form.is_valid():
+            video = form.save(commit=False)
+            title = video.video_title
+            video.user = request.user
+            video.slug = title.replace(' ', '-') + str(uuid.uuid4())
+            video.save()
+            return HttpResponseRedirect(reverse('home'))
+    return render(request, 'Stream_App/upload_videos.html', context={'form': form})
 
 
 def edit_videos(request):
